9a7f9503151ce17b792d8b9a763ccb09b3c030b7
Joshua Urdanabia <joshua.urdanabia@gmail.com>
2023-04-14 13:06:11 +0200

    envoi formulaire inscription vers DB

--- php/traitement.php
+++ php/traitement.php
@@ -1,7 +1,33 @@
 <?php 
 
+include 'request.php';
+
 if(isset($_POST['register-submit'])) {
-    var_dump($_POST);
-    // echo "ok c valide";
+    // var_dump($_POST);
+    // echo "Connexion validé !";
+
+    $userName = $_POST['name'];
+    $userEmail = $_POST['email'];
+    $userPassword = $_POST['password'];
+    var_dump($userEmail);
+    die();
+
+    $request = $db->prepare('INSERT INTO members VALUES (0, :name, :email, :password)');
+    $request->execute(
+        array(
+            "name" => $userName,
+            "email" => $userEmail,
+            "password" => $userPassword
+        )
+    );
+
+    $responce = $request->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($responce);
+
+} else {
+
+    echo 'Problème lors de la validation';
+
 }
+
 ?>
